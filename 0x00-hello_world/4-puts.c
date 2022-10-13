1932c265689397558569a9a37d345387aa5504a7
Chi-Favour <obumafavour019@gmail.com>
2022-10-13 14:16:15 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *main- serves as the entry point
  *
  *Return: zero means successful and not zero means error
- */
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
