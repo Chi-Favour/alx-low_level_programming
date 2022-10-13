4f938a91d32bdac9dec805afb7fee8487f8b9d0a
Chi-Favour <obumafavour019@gmail.com>
2022-10-13 14:39:00 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - serves as entry point
+ *
+ * Return:0 when successful but not 0 when there is error
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
